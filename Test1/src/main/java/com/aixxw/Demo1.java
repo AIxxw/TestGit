610fb799ab72b0f8bb59a55e420f96b3a97bab0a
AIxxw <aixxwaixxw@163.com>
2021-03-05 15:49:50 +0800

    2021/3/5/15:22

---
+++ Test1/src/main/java/com/aixxw/Demo1.java
@@ -0,0 +1,9 @@
+package com.aixxw;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Person person = new Person().setAge(12).setName("张三");
+        System.out.println(person);
+        System.out.println("ddd");
+    }
+}
---
+++ Test1/src/main/java/com/aixxw/Person.java
@@ -0,0 +1,11 @@
+package com.aixxw;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+public class Person {
+    private Integer age;
+    private String name;
+}
