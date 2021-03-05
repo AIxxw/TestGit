6e468a954c58885f8ff41f952bb806c2efda71ea
AIxxw <aixxwaixxw@163.com>
2021-03-05 16:10:48 +0800

    2021/3/5/15:54

---
+++ Test1/src/main/java/com/aixxw/Person1.java
@@ -0,0 +1,13 @@
+package com.aixxw;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Person1 {
+    private String name;
+    private String sex;
+    private Integer age;
+    private Date time;
+}
