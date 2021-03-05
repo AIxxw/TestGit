948c52efb76b6024e3860ff6fc7481a532566271
AIxxw <aixxwaixxw@163.com>
2021-03-05 16:22:14 +0800

    2021/3/5/15:54

---
+++ Test1/src/main/java/com/aixxw/Branch1619.java
@@ -0,0 +1,10 @@
+package com.aixxw;
+
+import java.util.stream.Stream;
+
+public class Branch1619 {
+    public static void main(String[] args) {
+        Stream.Builder<Object> builder = Stream.builder();
+
+    }
+}
